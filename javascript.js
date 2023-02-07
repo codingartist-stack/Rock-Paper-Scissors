2f9e879ea5278600c2b65642fdb39a88ec3046b6
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-02-06 21:11:37 -0700

    trying to make the playerSelection case insensitive and it's not working

--- javascript.js
+++ javascript.js
@@ -11,25 +11,25 @@ console.log("Welcome to Rock-Paper-Scissors")
 // if computer chooses scissors and user chooses paper, User loses
 // if computer chooses scissors and user chooses rock, User wins
 
-//result of the match are displayed in console.
+//result of the round displayed in console.
 
 let computerChoice
-let playerSelection = `Rock`;
+let playerSelection
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    //console.log(randomNumber);
-//using if
-    //if (randomNumber === 1) {
-        //computerChoice = `Rock`
-   // } 
-    //if (randomNumber === 2) {
-        //computerChoice = `Paper`
-    //}
-    //if (randomNumber === 3) {
-        //computerChoice = `Scissors`
-    //}
+        //console.log(randomNumber);
+        //using if
+        //if (randomNumber === 1) {
+            //computerChoice = `Rock`
+        // } 
+        //if (randomNumber === 2) {
+            //computerChoice = `Paper`
+        //}
+        //if (randomNumber === 3) {
+            //computerChoice = `Scissors`
+        //}
 
     //using swtich - I just wanted to see of I could do it in switch
     switch (randomNumber) {
@@ -47,6 +47,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerChoice) {
+
     if (playerSelection === computerChoice) {
         console.log(`It's a draw!`)
     }
