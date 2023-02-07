d9bd339dd3bb86aefa43f4d1594be1b16f2a32f4
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-02-06 19:36:56 -0700

    finised getting a random number and assigned number to be rock, paper, or scissors. Then started the function to play the game, but needed to stop.

--- javascript.js
+++ javascript.js
@@ -3,24 +3,23 @@ console.log("Welcome to Rock-Paper-Scissors")
 //Computer choose between rock, paper, or scissors
 //user chooses between rock, paper, or Scissors and make it case-insensitive
 
-// if computer chooses rock and user chooses rock,  it's a draw
+// if computer choice and user choice match,  it's a draw
 // if computer chooses rock and user chooses paper, User wins
 // if computer chooses rock and user chooses scissors, User loses
-// if computer chooses paper and user chooses paper, it's a draw
 // if computer chooses paper and user chooses scissors, User wins
 // if computer chooses paper and user chooses rock, User loses
-// if computer chooses scissors and user chooses scissors, it's a draw
 // if computer chooses scissors and user chooses paper, User loses
 // if computer chooses scissors and user chooses rock, User wins
 
 //result of the match are displayed in console.
 
-let ComputerChoice 
+let ComputerChoice
+let playerSelection = `Rock`;
 
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
-    
-    console.log(randomNumber);
+
+    //console.log(randomNumber);
 
     if (randomNumber === 1) {
         ComputerChoice = `Rock`
@@ -31,5 +30,10 @@ function getComputerChoice() {
     if (randomNumber === 3) {
         ComputerChoice = `Scissors`
     }
+
+    console.log(`Computer choose: ` + ComputerChoice);
 }
 
+function playRound(playerSelection, ComputerChoice)
+
+getComputerChoice();
